bd506a85458e7dad6aecd21d6f533562a8386d8b
vent-dev <55215656+vent-dev@users.noreply.github.com>
2023-01-21 16:20:14 +0000

    Added some extra styling and cleaned up code

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ function displayPrompt(){
 }
 
 function displayGrid(gridCount){
+    gridContainer.style.cssText = 'display: flex; border: 2rem solid red; border-radius: 1rem; flex-direction: row; width: fit-content;';
+
     for (let i = 0; i < gridCount; i++){
         const gridRow = document.createElement('div');
         for (let i = 0; i < gridCount; i++){
             const grid = document.createElement('p');
-            grid.style.margin = '0';
-            grid.style.padding = '.3rem';
-            grid.style.backgroundColor = 'grey';
+            grid.style.cssText = 'margin: 0; padding: .3rem; background-color: grey;';
             gridRow.appendChild(grid);
             grid.addEventListener('mousemove', () => { grid.style.backgroundColor = 'black' });
         }
